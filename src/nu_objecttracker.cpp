a33c9e5598acad91067d91a921974cc78af14f58
Jarvis Schultz <jarvis@u.northwestern.edu>
2011-03-08 21:11:48 -0600

    Working statistical filter.  This does not work!

--- src/nu_objecttracker.cpp
+++ src/nu_objecttracker.cpp
@@ -27,9 +27,11 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/features/feature.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/point_cloud_conversion.h>
+
 
 
 //---------------------------------------------------------------------------
@@ -73,14 +75,17 @@ public:
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr
       cloud (new pcl::PointCloud<pcl::PointXYZ> ()),
-      cloud_voxel (new pcl::PointCloud<pcl::PointXYZ> ()),
-      cloud_filtered_x (new pcl::PointCloud<pcl::PointXYZ> ()),
-      cloud_filtered_y (new pcl::PointCloud<pcl::PointXYZ> ()),
-      cloud_filtered_z (new pcl::PointCloud<pcl::PointXYZ> ());
+	cloud_voxel (new pcl::PointCloud<pcl::PointXYZ> ()),
+	cloud_filtered_x (new pcl::PointCloud<pcl::PointXYZ> ()),
+	cloud_filtered_y (new pcl::PointCloud<pcl::PointXYZ> ()),
+	cloud_filtered_z (new pcl::PointCloud<pcl::PointXYZ> ()),
+	cloud_stat (new pcl::PointCloud<pcl::PointXYZ> ()),
+	cloud_stat_outlier (new pcl::PointCloud<pcl::PointXYZ> ());
+    
 
     // convert pcl2 message to pcl object
     pcl::fromROSMsg(*scan,*cloud);  
-    pcl::fromROSMsg(*cloud_voxel_pcl2,*cloud_voxel);
+    // pcl::fromROSMsg(*cloud_voxel_pcl2,*cloud_voxel);
 
     datacount++;  // we got a new set of data so increment datacount
     //printf("%i\n", datacount);
@@ -91,12 +96,12 @@ public:
     float ypos = 0.0;
     float zpos = 0.0;
 
-    //ros::Time tstart = ros::Time::now();
-    //std::cout << "start time:  " << tstart << "\n ";
+    // ros::Time tstart = ros::Time::now();
+    // std::cout << "start time:  " << tstart << "\n ";
 
     // pass through filter
     pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud(cloud_voxel);
+    pass.setInputCloud(cloud);
     pass.setFilterFieldName("x");
     pass.setFilterLimits(-1.0, 1.0);
     pass.filter(*cloud_filtered_x);
@@ -108,14 +113,26 @@ public:
 
     pass.setInputCloud(cloud_filtered_y);
     pass.setFilterFieldName("z");
-    pass.setFilterLimits(1, 1.8);
+    pass.setFilterLimits(.75, 2.5);
     pass.filter(*cloud_filtered_z);
 
-    pcl::toROSMsg(*cloud_filtered_z, *object1_cloud);
-    cloudpub_[0].publish(object1_cloud);
-    //cloudpub_[1].publish(hands.hands[1].handcloud);
+    // Perform statistical filtering:
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor2;
+    sor2.setInputCloud (cloud_filtered_z);
+    sor2.setMeanK (50);
+    sor2.setStddevMulThresh (0.001);
+    sor2.filter (*cloud_stat);
 
-    pcl::compute3DCentroid(*cloud_filtered_z, centroid);
+    sor2.setNegative(true);
+    sor2.filter(*cloud_stat_outlier);
+
+    pcl::toROSMsg(*cloud_stat, *object1_cloud);
+    cloudpub_[0].publish(object1_cloud);
+    
+    pcl::toROSMsg(*cloud_stat_outlier, *object2_cloud);
+    cloudpub_[1].publish(object2_cloud);
+
+    pcl::compute3DCentroid(*cloud_stat, centroid);
     xpos = centroid(0);
     ypos = centroid(1);
     zpos = centroid(2);
@@ -127,8 +144,8 @@ public:
     static tf::TransformBroadcaster br;
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "openni_depth_optical_frame", "object1"));
 
-    //ros::Time tstop = ros::Time::now();
-    //std::cout << "finish time:  " << tstop << "\n ";
+    // ros::Time tstop = ros::Time::now();
+    // std::cout << "finish time:  " << tstop << "\n ";
 
     /*
     printf("X: %f\n", xpos);
